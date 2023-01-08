c45bd1e6da5a61f63d6126df8aeb25c9c3fc779f
Stan Zaytsev <stan.zaytsev@onsend.com>
2023-01-08 13:25:16 +1300

    Add an example of a non-destructured assignment

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    {{ isLoading }} <!-- incorrect -->
-    {{ data }} <!-- incorrect -->
+    {{ query.isLoading }} <!-- semi-correctly inferred as DistributiveOmit but `isLoading` is Unresolved -->
+    {{ query.data }} <!-- semi-correctly inferred as DistributiveOmit but `data` is unresolved -->
+    {{ isLoading }} <!-- incorrectly inferred as `any` -->
+    {{ data }} <!-- incorrectly inferred as `any` -->
     {{ bacon }} <!-- correctly inferred as `number` -->
   </div>
 </template>
@@ -9,6 +11,10 @@
 <script lang="ts" setup>
 import { useQuery } from '@tanstack/vue-query'
 import { ref } from 'vue';
+
+const query = useQuery({
+  queryFn: () => Promise.resolve<number>(1)
+})
 
 const { isLoading, data } = useQuery({
   queryFn: () => Promise.resolve<number>(1)
