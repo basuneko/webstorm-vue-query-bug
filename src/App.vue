377ab596e21e17652eb4886205aa02d9ea7973af
Stan Zaytsev <stan.zaytsev@onsend.com>
2023-01-08 13:32:08 +1300

    Add examples with explicitly-declared query types

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div>
     {{ query.isLoading }} <!-- semi-correctly inferred as DistributiveOmit but `isLoading` is Unresolved -->
     {{ query.data }} <!-- semi-correctly inferred as DistributiveOmit but `data` is unresolved -->
+
+    {{ explicitlyTypedQuery.isLoading }} <!-- semi-correctly inferred as DistributiveOmit but `isLoading` is Unresolved -->
+    {{ explicitlyTypedQuery.data }} <!-- semi-correctly inferred as DistributiveOmit but `data` is unresolved -->
+
     {{ isLoading }} <!-- incorrectly inferred as `any` -->
     {{ data }} <!-- incorrectly inferred as `any` -->
     {{ bacon }} <!-- correctly inferred as `number` -->
@@ -13,10 +17,17 @@ import { useQuery } from '@tanstack/vue-query'
 import { ref } from 'vue';
 
 const query = useQuery({
+  queryKey: ['key'],
+  queryFn: () => Promise.resolve<number>(1)
+})
+
+const explicitlyTypedQuery = useQuery<number, unknown, number>({
+  queryKey: ['explicit key'],
   queryFn: () => Promise.resolve<number>(1)
 })
 
 const { isLoading, data } = useQuery({
+  queryKey: ['destructured assignment'],
   queryFn: () => Promise.resolve<number>(1)
 })
 
